bebdade1864751dd5cdaa4fe480acaaa072d0efb
Ken Wu <abawwu@gmail.com>
2015-12-09 01:58:31 +0800

    Adjust xmobar execution and implement M-/

--- machines/ubuntu/xmonad.hs
+++ machines/ubuntu/xmonad.hs
@@ -1,26 +1,64 @@
+{-# LANGUAGE DeriveDataTypeable #-}
 import XMonad
-import XMonad.Hooks.ManageDocks
+import XMonad.Actions.WindowGo (runOrRaiseNext)
+import XMonad.Hooks.ManageDocks (manageDocks, avoidStruts)
+import XMonad.Layout.NoBorders (smartBorders)
+import XMonad.Util.EZConfig (additionalKeysP)
+import XMonad.Util.Run (spawnPipe)
 import qualified XMonad.Hooks.DynamicLog as DL
-import XMonad.Util.EZConfig
-import XMonad.Actions.WindowGo
+import qualified XMonad.Util.ExtensibleState as XS
 
-main = xmonad myConfig
+import Data.Maybe (isNothing)
+import System.IO (hPutStrLn, Handle, stderr)
 
+data MyState = MyState
+	{ msWindows :: [Window] -- ^ most recent windows
+	} deriving Typeable
 
-myConfig = defaultConfig
-	{ modMask = mod5Mask
-	, manageHook = myManageHook
-	, layoutHook = myLayoutHook
-	, logHook = DL.dynamicLogWithPP DL.xmobarPP
-			{ DL.ppTitle = DL.xmobarColor "green" "" . DL.shorten 50
+instance ExtensionClass MyState where
+	initialValue = MyState
+			{ msWindows = []
 			}
-	} `additionalKeysP` myKeyBindings
 
-myManageHook = manageDocks <+> manageHook defaultConfig
-myLayoutHook = avoidStruts $ layoutHook defaultConfig
+main = do
+	xmobarH <- spawnPipe "xmobar"
+	xmonad $ defaultConfig
+		{ modMask = mod5Mask
+		, focusFollowsMouse = False
+		, terminal = "urxvt"
+		, manageHook = manageDocks <+> manageHook defaultConfig
+		, layoutHook = myLayoutHook
+		, logHook = updateXmobar xmobarH >> updateLastWindow
+		} `additionalKeysP` myKeyBindings
 
 myKeyBindings =
 	[ ("M-b", chrome)
+	, ("M-/", focusLastWindow)
 	]
 
+myLayoutHook = avoidStruts $ standardLayout
+  where
+	standardLayout = smartBorders $ layoutHook defaultConfig
+
 chrome = runOrRaiseNext "google-chrome" (className =? "google-chrome")
+
+updateXmobar :: Handle -> X ()
+updateXmobar h = DL.dynamicLogWithPP DL.xmobarPP
+			{ DL.ppTitle = DL.xmobarColor "green" "" . DL.shorten 50
+			, DL.ppOutput = hPutStrLn h
+			}
+
+updateLastWindow :: X ()
+updateLastWindow = withFocused $ XS.modify . go
+  where
+	go w s = s { msWindows = update w (msWindows s) } 
+	-- remember at most 2 windows
+	update w (x:y) = if w == x then x:y else w:x:y
+	update w [] = w:[]
+
+focusLastWindow :: X ()
+focusLastWindow = do
+	ws <- XS.gets msWindows
+	case ws of
+		f:w:_ -> focus w
+		_ -> return ()
