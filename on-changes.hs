79864dde8ffc7116be0804d84ec359b193eb6c48
ken.wu <ken.wu@ubnt.com>
2016-02-05 09:50:46 +0800

    on-changes.hs enhancement and refactorying
    - implemented -i to ignore editor generated files
    - refactorying a little bit for readability

--- on-changes.hs
+++ on-changes.hs
@@ -15,18 +15,23 @@ import           System.FSNotify
 import           Turtle
 
 data Config = Config
-  { cMonitoredDirs :: [FilePath] -- ^ The directories to monitor for changes
-  , cSuffixes      :: [Text]     -- ^ Suffixes for my interested files
-  , cCommand       :: Text       -- ^ The shell command to run on file changes in a directory
+  { cMonitoredDirs     :: [FilePath]
+  , cSuffixes        :: [Text]
+  , cIgnoreEditorFiles :: Bool
+  , cCommand           :: Text
   }
   deriving (Show)
 
+description :: Description
+description = "Execute a shell command on file chnages or on reciving a newline from stdin."
+
 parser :: Parser Config
-parser = Config <$> some (optPath "dir" 'd' (Specific "The directories to monitor changes in. You could specify this multiple times."))
+parser = Config <$> many (optPath "dir" 'd' (Specific "The directories to monitor changes in. You could specify this multiple times."))
                 <*> many (optText "suffix" 's' (Specific "Only changes for files matching the given suffixes trigger the command."))
+                <*> switch "--ignore-editor-files" 'i' (Specific "Ignoring some files created by editors: .*.swp, .*.swx, *.swpx .#*")
                 <*> (fmap T.unwords ( some $ argText "command..." (Specific "The shell command to execute on file changes")))
 
-main = do cfg@(Config dirs _ cmd) <- options "Execute a shell command on file chnages" parser
+main = do cfg@(Config dirs _ _ cmd) <- options description parser
           print cfg
           checkDirs dirs
           c <- setupTriggerThread cfg
@@ -38,24 +43,40 @@ main = do cfg@(Config dirs _ cmd) <- options "Execute a shell command on file ch
     checkDirs dirs = forM_ dirs $ \d -> do ok <- testdir d
                                            unless ok $ die ("No such directory: " <> format fp d)
 
-setupTriggerThread (Config _ _ cmd) =
+setupTriggerThread (Config _ _ _ cmd) =
   do c <- newTChanIO
      forkIO $ forever $ do
+       display $ "Command to exeucte when triggered: " <> cmd
        msgs <- atomically $ some (readTChan c)
-       mapM_ echo msgs
+       mapM_ display msgs
        shell cmd empty
      return c
+  where
+    display msg = echo bar >> echo msg >> echo bar
+    bar = mconcat $ replicate 80 "="
+
+eventPath' :: IsString a => Event -> a
+eventPath' (Added p _) = fromString p
+eventPath' (Modified p _) = fromString p
+eventPath' (Removed p _) = fromString p
+
+shouldTrigger :: Config -> Event -> Bool
+shouldTrigger (Config _ suffixes ignoreEditorFiles _) e =
+  and [ null suffixes || matchesSuffixes
+      , not $ ignoreEditorFiles && matchesEditorFiles
+      ]
+  where
+    p = eventPath' e
+    Right p' = toText $ filename $ eventPath' e
+    matchesSuffixes = any (`T.isSuffixOf` p') suffixes
+    matchesEditorFiles = or [ "." `T.isPrefixOf` p' && any (`T.isSuffixOf` p') [".swp", ".swx", ".swpx" ]
+                              -- ^ VIM
+                            , ".#" `T.isPrefixOf` p'
+                              -- ^ Emacs
+                            ]
 
 setupFileTrigger manager cfg trigger =
-    forM_ dirs $ \d -> watchTree manager d pred trigger'
+    forM_ dirs $ \d -> watchTree manager d (shouldTrigger cfg) trigger'
   where
-    pred e = let (Config _ suffixes _) = cfg
-                 p = path e
-             in if null suffixes
-                then True
-                else any (`isSuffixOf` p) $ map T.unpack suffixes
-    path (Added p _) = p
-    path (Modified p _) = p
-    path (Removed p _) = p
-    trigger' e = trigger $ T.pack $ "Triggered by " ++ path e ++ "!"
+    trigger' e = trigger $ "Triggered by " <> eventPath' e <> "!"
     dirs = map (T.unpack . format fp) (cMonitoredDirs cfg)
